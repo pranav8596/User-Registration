75cfb2819b4c78994c587f103159a961c1837395
NITIN <pige786123@gmail.com>
2020-04-23 00:52:22 +0530

    ADDED: Checked the validation for User's first name

---
+++ User Registration Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User_Registration_Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO USER REGISTRATION PROBLEM");
+        }
+    }
+}
